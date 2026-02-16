339bc08cc973160f6bd94b36a430207012d2f644
Tamil Selvan <tv3664@srmist.edu.in>
2026-02-16 13:34:33 +0530

    UC1: Print OOPS banner to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * Prints OOPS banner to console
+ *
+ * @author Tamil Selvan
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println(" *****   *****  *       ***** ");
+
+    }
+}
