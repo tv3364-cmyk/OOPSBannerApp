f4e2abf398dc2c15a0e596d6252f65d1ff8abcf3
Your Name <your_uni_or_personal_email>
2026-03-17 08:33:56 +0530

    UC4: Display Hello with multiple command-line arguments

---
+++ HelloAppUC4.java
@@ -0,0 +1,15 @@
+public class HelloAppUC4 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+
+        }
+    }
+}
